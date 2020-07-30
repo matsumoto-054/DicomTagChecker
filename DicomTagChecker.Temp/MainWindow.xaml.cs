c99b680619ad8cf7a18856fddc703d257a6f29ba
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-30 10:18:09 +0900

    Closingイベントをひとまず用意

--- DicomTagChecker.Temp/MainWindow.xaml.cs
+++ DicomTagChecker.Temp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -37,6 +38,16 @@ namespace DicomTagChecker.Temp
             {
                 FolderPathTextBox.Text = dialog.FileName;
             }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
