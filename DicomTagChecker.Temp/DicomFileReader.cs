c552981037b0f03bf590e1e2f6ca5add1b8cc579
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-30 10:27:59 +0900

    UI調整

---
+++ DicomTagChecker.Temp/DicomFileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DicomTagChecker.Temp
+{
+    public class DicomFileReader
+    {
+        //ファイルの移動（Temporaryへ）
+
+        //ファイル読込（Logへの書き出し）
+
+        //Validate
+
+        //引っかかったものをcsv出力
+    }
+}
