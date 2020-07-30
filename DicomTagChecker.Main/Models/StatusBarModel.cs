a9095d4433436fc4f1bb0b7cee64c35b35fca05d
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-30 09:22:00 +0900

    途中まで作成分を復活

---
+++ DicomTagChecker.Main/Models/StatusBarModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+
+namespace DicomTagChecker.Main.Models
+{
+    public class StatusBarModel : BindableBase
+    {
+        public string _status = "準備完了";
+
+        public string Status
+        {
+            get { return _status; }
+            set { SetProperty(ref _status, value); }
+        }
+
+        public StatusBarModel()
+        {
+        }
+
+        public void ChangeStatus(string status)
+        {
+            this.Status = status;
+        }
+    }
+}
---
+++ DicomTagChecker/DicomTagChecker.Main/Models/FolderPathModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DicomTagChecker.Main.Models
+{
+    class FolderPathModel
+    {
+    }
+}
